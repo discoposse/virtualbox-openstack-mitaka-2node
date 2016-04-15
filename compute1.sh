1ff3a9029acfb2b65b63c1b03fa6ad433547e41d
discoposse <eric@discoposse.com>
2016-04-15 15:50:46 -0400

    scratch file and some config files

--- compute1.sh
+++ compute1.sh
@@ -7,8 +7,8 @@
 export DEBIAN_FRONTEND=noninteractive
 echo "set grub-pc/install_devices /dev/sda" | debconf-communicate
 
-sudo apt-get update && sudo apt-get upgrade -y 
-sudo apt-get install -y git vim openssh-server software-properties-common python-pip 
-sudo add-apt-repository cloud-archive:mitaka
-sudo apt-get update && apt-get dist-ugrade -y
-sudo reboot
+#sudo apt-get update && sudo apt-get upgrade -y 
+#sudo apt-get install -y git vim openssh-server software-properties-common python-pip 
+#sudo add-apt-repository cloud-archive:mitaka -y
+#sudo apt-get update && sudo apt-get dist-upgrade -y
+#sudo reboot
